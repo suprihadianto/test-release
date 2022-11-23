84484d0df366fa7f50bc711e0a550c2092b32ae1
Supri Hadianto <bintanEMAIL@gmail.com>
2022-11-23 12:52:44 +0700

    Update build-linux.sh

--- github-actions/build-linux.sh
+++ github-actions/build-linux.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 function build() {
-  pwd
-  
   cd "${GITHUB_WORKSPACE}" || exit
-  
-  pwd
 
   mkdir "${GITHUB_WORKSPACE}/bin"
 
-  
-  ls "${GITHUB_WORKSPACE}/target/x86_64-unknown-linux-musl/release"
-  echo "${RELEASE_NAME}"
+  cp "${GITHUB_WORKSPACE}/output/${RELEASE_NAME}" \
+    "${GITHUB_WORKSPACE}"
 }
 build
