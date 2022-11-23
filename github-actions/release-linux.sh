891f7a2a2b240acceca8274ae3b8a697271b02ec
Supri Hadianto <bintanEMAIL@gmail.com>
2022-11-23 13:02:36 +0700

    Update release-linux.sh

--- github-actions/release-linux.sh
+++ github-actions/release-linux.sh
@@ -39,14 +39,8 @@ function publish() {
     --data-binary @${binary} \
     -H 'Content-Type: application/octet-stream' \
     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-    "${upload_url}?name=${release_name}-linux_amd64"
+    "${upload_url}?name=${release_name}-linux_amd64.zip"
 
-  curl \
-    -X POST \
-    --data "$checksum" \
-    -H 'Content-Type: text/plain' \
-    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-    "${upload_url}?name=${release_name}-linux_amd64.sha256sum"
 }
 
 publish "${GITHUB_WORKSPACE}"'/'"${RELEASE_NAME}"
